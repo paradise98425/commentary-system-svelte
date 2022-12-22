17a3d4873b0962ce5244e1cca27a7b3cf27adc72
paradise98425 <paradise98425@gmail.com>
2022-12-22 23:05:38 +0100

    includes the working playwright test configuration

--- tests-examples/test-code.spec.js
+++ tests-examples/test-code.spec.js
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test';
 
 test('Read more link', async ({ page }) => {
-	await page.goto('/');
+	await page.goto('http://localhost:5173/');
 	// Click the get started link.
 	await page.getByRole('link', { name: 'Read more' }).click();
 	// Expects the URL to contain intro.
@@ -10,14 +10,14 @@ test('Read more link', async ({ page }) => {
 
 test('like button test', async ({ page }) => {
 	// goes to the home page
-	await page.goto('/');
+	await page.goto('http://localhost:5173/');
 	// clicks on the heart button
 	await page.getByRole('button', { name: 'Icon for like- an outlined heart' }).click();
 });
 
 test('Leave your comment test', async ({ page }) => {
 	// goes to the home page
-	await page.goto('/');
+	await page.goto('http://localhost:5173/');
 	// focus on the inputfield to add comment
 	await page.getByPlaceholder('Leave your comment').click();
 	// enables to write comment
@@ -28,7 +28,7 @@ test('Leave your comment test', async ({ page }) => {
 
 test('click the reply button', async ({ page }) => {
 	// goes to the home page
-	await page.goto('/');
+	await page.goto('http://localhost:5173/');
 	//since the Reply button is displayed only on hover, gotta hover first before selecting the button
 	await page.getByText('Mayanna Jill Luxury in design! Amazing. Reply').hover();
 	// finds and clicks the button for reply
@@ -37,7 +37,7 @@ test('click the reply button', async ({ page }) => {
 
 test('Leave your comment in reply thread', async ({ page }) => {
 	// goes to the home page
-	await page.goto('/');
+	await page.goto('http://localhost:5173/');
 	// focus on the  inputfield for reply thread
 	await page.getByPlaceholder('Leave your comment').first().click();
 	// enables to write the thread comment
